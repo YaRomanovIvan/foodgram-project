b8b950d738f66fcb8a923d99aaaff65fdbe73ac4
YaRomanovIvan <romanov19972@yandex.ru>
2021-05-21 10:02:09 +0300

    fix

--- apps/recipes/forms.py
+++ apps/recipes/forms.py
@@ -7,68 +7,72 @@ from .service import add_ingredients_to_recipe
 
 class RecipeForm(forms.ModelForm):
     tags = forms.ModelMultipleChoiceField(
-        label='Теги',
+        label="Теги",
         queryset=Tag.objects.all(),
         widget=forms.CheckboxSelectMultiple,
     )
     ingredients = forms.CharField(
         required=False,
-        label='Ингредиенты',
-        widget=forms.TextInput(attrs={'id': 'nameIngredient'}),
+        label="Ингредиенты",
+        widget=forms.TextInput(attrs={"id": "nameIngredient"}),
     )
 
     class Meta:
         model = Recipe
         fields = (
-            'title',
-            'tags',
-            'ingredients',
-            'cooking_time',
-            'text',
-            'image',
+            "title",
+            "tags",
+            "ingredients",
+            "cooking_time",
+            "text",
+            "image",
         )
         labels = {
-            'title': 'Название рецепта',
-            'text': 'Рецепт',
-            'cooking_time': 'Время приготовления',
-            'image': 'Загрузить фото',
+            "title": "Название рецепта",
+            "text": "Рецепт",
+            "cooking_time": "Время приготовления",
+            "image": "Загрузить фото",
         }
         widgets = {
-            'cooking_time': forms.TextInput(),
+            "cooking_time": forms.TextInput(),
         }
 
     def clean_ingredients(self):
         ingredients = list(
             zip(
-                self.data.getlist('nameIngredient'),
-                self.data.getlist('valueIngredient'),
+                self.data.getlist("nameIngredient"),
+                self.data.getlist("valueIngredient"),
             ),
         )
-        print('Ингредиенты: ', ingredients)
+
         if not ingredients:
-            raise forms.ValidationError('Отсутствуют ингредиенты')
+            raise forms.ValidationError("Отсутствуют ингредиенты")
 
         all_ingredients = Ingredient.objects.all()
         ingredients_clean = []
         for name, quantity in ingredients:
             if not all_ingredients.filter(title=name):
                 raise ValidationError(
-                    f'В базе данных нет ингредиента "{name}".')
+                    f'В базе данных нет ингредиента "{name}".'
+                )
             if int(quantity) < 1:
                 raise forms.ValidationError(
-                    f'Исправьте количество ингредиента "{name}"')
+                    f'Исправьте количество ингредиента "{name}"'
+                )
             else:
-                ingredients_clean.append({
-                    'title': name,
-                    'quantity': quantity,
-                })
+                ingredients_clean.append(
+                    {
+                        "title": name,
+                        "quantity": quantity,
+                    }
+                )
         return ingredients_clean
 
     def save(self, commit=True):
         instance = super().save(commit=False)
         instance.save()
-        ingredients = self.cleaned_data['ingredients']
-        self.cleaned_data['ingredients'] = []
+        ingredients = self.cleaned_data["ingredients"]
+        self.cleaned_data["ingredients"] = []
         self.save_m2m()
         add_ingredients_to_recipe(self.instance, ingredients)
 
--- apps/recipes/templatetags/user_filters.py
+++ apps/recipes/templatetags/user_filters.py
@@ -5,12 +5,12 @@ from apps.users.models import Follow
 
 register = Library()
 
-VARIANTS = ('рецепт', 'рецепта', 'рецептов')
+VARIANTS = ("рецепт", "рецепта", "рецептов")
 
 
 @register.filter
 def addclass(field, css):
-    return field.as_widget(attrs={'class': css})
+    return field.as_widget(attrs={"class": css})
 
 
 @register.filter
@@ -30,26 +30,26 @@ def is_purchase(recipe, user):
 
 @register.simple_tag(takes_context=True)
 def param_replace(context, **kwargs):
-    query_string = context['request'].GET.copy()
-    if 'page' in kwargs:
-        query_string['page'] = kwargs.get('page')
+    query_string = context["request"].GET.copy()
+    if "page" in kwargs:
+        query_string["page"] = kwargs.get("page")
 
     return query_string.urlencode()
 
 
 @register.simple_tag(takes_context=True)
 def manage_tags(context, **kwargs):
-    tag = kwargs['tag']
-    query_string = context['request'].GET.copy()
-    tags = query_string.getlist('tags')
+    tag = kwargs["tag"]
+    query_string = context["request"].GET.copy()
+    tags = query_string.getlist("tags")
     if tag in tags:
         tags.remove(tag)
     else:
         tags.append(tag)
-    query_string.setlist('tags', tags)
+    query_string.setlist("tags", tags)
 
-    if 'page' in query_string:
-        query_string.pop('page')
+    if "page" in query_string:
+        query_string.pop("page")
 
     return query_string.urlencode()
 
@@ -58,23 +58,22 @@ def manage_tags(context, **kwargs):
 def declination(number, args):
     mod_10 = number % 10
     mod_100 = number % 100
-    string = ''
+    string = ""
     if mod_10 == 1 and mod_100 != 11:
         string += VARIANTS[0]
-    elif mod_10 >= 2 and mod_10 <= 4 \
-            and (mod_100 < 10 or mod_100 >= 20):
+    elif mod_10 >= 2 and mod_10 <= 4 and (mod_100 < 10 or mod_100 >= 20):
         string += VARIANTS[1]
     else:
         string += VARIANTS[2]
-    return f'{number} { string}'
+    return f"{number} { string}"
 
 
 @register.simple_tag
 def full_page(request, page_num):
     path = request.get_full_path()
-    current_page = request.GET.get('page')
-    if 'tags' in path and 'page' in path:
-        return path.replace(f'page={current_page}', f'page={page_num}')
-    if 'tags' in path and 'page' not in path:
-        return f'?page={page_num}&{path[2:]}'
-    return f'?page={page_num}'
+    current_page = request.GET.get("page")
+    if "tags" in path and "page" in path:
+        return path.replace(f"page={current_page}", f"page={page_num}")
+    if "tags" in path and "page" not in path:
+        return f"?page={page_num}&{path[2:]}"
+    return f"?page={page_num}"
